8cc7fe4d120fb57c9ce346e7aabc610a727a6860
Miguel <morillomiguel2707@gmail.com>
2026-06-30 23:33:11 -0400

    Improved argument handling and added option to clear cache.

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import argparse
+import os
+import shutil
+import sys
 from server import start_server
 
-if __name__ == "__main__":
-
-    # Description
-    parser = argparse.ArgumentParser(
-        description="Caching Server"
+def main():
+    parser = argparse.ArgumentParser(description="CLI para el Servidor Proxy Caché.")
+    
+    # Clear cache option
+    parser.add_argument(
+        "--clear-cache", 
+        action="store_true", 
+        help="Delete all cache store"
     )
 
     # Port
-    parser.add_argument(
-        "--port", type=int, default=5000, help="Port where the proxy will run"
-    )
-
+    parser.add_argument("--port", type=int, default=5000)
     # URL
-    parser.add_argument(
-        "--origin",
-        type=str,
-        required=True,
-        help="Origin server URL (ej. https://getcomposer.org)",
-    )
+    parser.add_argument("--origin", type=str, required="--clear-cache" not in sys.argv)
 
     args = parser.parse_args()
 
-    ORIGIN_URL = args.origin # Global variable
+    if args.clear_cache:
+        if os.path.exists(".cache"):
+            shutil.rmtree(".cache")
+            print("Cache cleared")
+        else:
+            print("Cache is empty")
+        return  
 
-    start_server(ORIGIN_URL, args.port)
+    start_server(origin_url=args.origin, port_input=args.port)
+
+if __name__ == "__main__":
+    main()
